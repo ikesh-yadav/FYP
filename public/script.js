3b661f73d9a8f3f2436e4935ba2837c98d13d6e6
ikesh-yadav <ikeshyadav1@gmail.com>
2021-06-08 18:12:39 +0530

    wworking but fetch disabled

--- public/script.js
+++ public/script.js
@@ -4,6 +4,8 @@ jQuery(document).ready(function($){
 	$('#main-form').on('submit', (event)=>{
 		event.preventDefault();
 		console.log("submit event");
+
+		//trigger the animation - open modal window
 		var actionBtn = $('#process-btn'),
 		scaleValue = retrieveScale(actionBtn.next('.cd-modal-bg'));
 		
@@ -34,19 +36,7 @@ jQuery(document).ready(function($){
 
 
 
-	//trigger the animation - open modal window
-	// $('[data-type="modal-trigger"]').on('click', function(){
-	// 	var actionBtn = $(this),
-	// 		scaleValue = retrieveScale(actionBtn.next('.cd-modal-bg'));
-		
-	// 	actionBtn.addClass('to-circle');
-	// 	actionBtn.next('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-	// 		animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
-	// 	});
-
-	// 	//if browser doesn't support transitions...
-	// 	if(actionBtn.parents('.no-csstransitions').length > 0 ) animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
-	// });
+	
 
 	//trigger the animation - close modal window
 	$('.container .cd-modal-close').on('click', function(){
